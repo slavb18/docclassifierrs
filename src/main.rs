dd7a1f3045927458c953d12b0dfdc61b8b7da973
Vjacheslav Borisov <slavb@ilb.ru>
2022-04-07 15:36:21 +0400

    added crop image

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
 
 fn img_to_text(path: &str) {
 
-  let img = image::open(path).unwrap();
+  let mut img = image::open(path).unwrap();
 
   img.thumbnail(1700,1700);
 
@@ -23,7 +23,17 @@ fn img_to_text(path: &str) {
 
   // The color method returns the image's `ColorType`.
   println!("{:?}", img.color());
+  let width=img.width();
+  let height = img.height();
+
+  let img2 = image::imageops::crop(&mut img,40,40,width-80,height-80);
+
+  // The dimensions method returns the images width and height.
+  println!("crop dimensions {:?}", img2.dimensions());
+
 
   // Write the contents of this image to the Writer in PNG format.
-  img.save("test/result.png").unwrap();
+  //img.save("test/result.png").unwrap();
+
+  //img2.to_image().save("test/result.png").unwrap();
 }
